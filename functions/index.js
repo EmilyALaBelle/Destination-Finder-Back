fcfcf1da44212ded87415274e12cdbd11af9b4ca
Emily LaBelle <emilylabelle8@gmail.com>
2022-12-06 12:20:32 -0500

    added user sign in

---
+++ functions/index.js
@@ -0,0 +1,21 @@
+import functions from 'firebase-functions'
+import express from 'express'
+import cors from 'cors'
+import { createNewDestination, getAllDestinations, updateDestination, postSpecificDestination, likeDestination } from './src/destination.js'
+import { addNewUser, userLogin, updateUser } from './src/user.js'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/destinations',  getAllDestinations)
+app.post('/destinations',  createNewDestination)
+app.patch('/destinations/:destinationId',  updateDestination)
+app.post('/destination',  postSpecificDestination)
+//app.delete('destinations/:destinationId', await deleteDestination)
+app.post('/login', userLogin)
+app.post('/signup', addNewUser)
+app.patch('/update/:userId', updateUser)
+app.patch('/like/:destinationId', likeDestination)
+
+export const api = functions.https.onRequest(app)
